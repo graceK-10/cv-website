1d0fe50dc63f062b7a523e574c1391a8d20aed33
graceK-10 <kipagrace@gmail.com>
2024-04-27 07:20:11 +0200

    education & experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '4': '4px',
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+        DEFAULT: theme('colors.gray.300', 'currentColor'),
+        // Add more colors as needed
+      }),
+},
   },
   plugins: [
         // ...
