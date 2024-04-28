314c5914a854c9be69010c939a28e95ba7da91a9
graceK-10 <kipagrace@gmail.com>
2024-04-28 08:43:50 +0200

    final changes for now

---
+++ main.js
@@ -0,0 +1,7 @@
+// TYPING ANIMATION
+let typed = new Typed(".typing", {
+    strings:["", "Web Developer", "Web Designer" , "UI/UX Designer"],
+    typeSpeed:110,
+    BackSpeed:60,
+    loop:true
+})
