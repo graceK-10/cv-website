a51709cad23403166fc86f0af54f9136a1495d88
graceK-10 <kipagrace@gmail.com>
2024-05-22 16:57:06 +0200

    updates

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ let typed = new Typed(".typing", {
     BackSpeed:60,
     loop:true
 })
+
+// Function to send the email
+// function sendEmail() {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "kipagrace@gmail.com",
+//         Password: "your-password",
+//         To: 'kipagrace@gmail.com',
+//         From: document.getElementById("email").value,
+//         Subject: "Recruitment",
+//         Body: "And this is the body",
+//         SecureToken: "your-secure-token" 
+//     }).then(
+//       message => alert("Email sent successfully: " + message),
+//       error => alert("Failed to send email: " + error)
+//     );
+// }
+
